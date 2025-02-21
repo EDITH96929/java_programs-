e3d2425643538387dc7056ffd5146b8053355061
sunil kumar swain <240720100076@centurionuniv.edu.in>
2025-02-21 22:59:45 +0530

    commit java programs

---
+++ pattern2.java
@@ -0,0 +1,21 @@
+package cutm;
+
+public class pattern2 {
+
+    static int rows = 4;
+    static int columns = 4;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
